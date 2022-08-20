031d4e0f50e6caf4a504fb1da2df542808c5c61f
FarmBera <101335222+FarmBera@users.noreply.github.com>
2022-08-20 17:32:39 +0900

    Delete

--- src/components/Goodbye.jsx
+++
@@ -1,5 +0,0 @@
-export default function GoodBye () {
-    return (
-        <h1>Good Bye!</h1>
-    )
-}
--- src/components/Hello.jsx
+++
@@ -1,5 +0,0 @@
-export default function Hello (props) {
-    return (
-        <h1>Hello {props.name}ui age is{props.age}</h1>
-    )
-}
